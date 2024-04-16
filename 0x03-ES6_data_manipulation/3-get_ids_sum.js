7a90b217a981c8ad2b080736f0a87b0e7058e75d
Ayoub Baalla <onlyayoub565@gmail.com>
2024-04-16 10:44:00 +0000

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  return students.reduce((agg, student) => agg + student.id, 0);
+export default function getStudentIdsSum(students) {
+  return students.reduce((agg, student) => agg + student.id, 0);
+export default function getStudentIdsSum(students) {
+  return students.reduce((agg, student) => agg + student.id, 0);
+}}}
